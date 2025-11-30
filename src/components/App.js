bb12fb4f48b4296eeae15bc1ce7ca960fcf77fab
vedantkherade <vedantkherade76@gmail.com>
2025-11-30 22:45:14 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import Login from "./Login";
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
   return (
     <div>
         {/* Do not remove the main div */}
+        {isLoggedIn ? (
+        <h2>You are logged in!</h2>
+      ) : (
+        <Login isLoggedIn={isLoggedIn} onLogin={handleLogin} />
+      )}
     </div>
   )
 }
---
+++ src/components/Login.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Login({ isLoggedIn, onLogin }) {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onLogin(); 
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h3>Login Form</h3>
+      <input type="text" placeholder="Enter username" required />
+      <input type="password" placeholder="Enter password" required />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default Login;
